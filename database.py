9316952492147df4cce76ceef594075b58b10923
gabriel.mango <gabriel.mango@defensoria.mg.def.br>
2025-01-16 16:58:59 -0300

    adicionado consistencia de dados na tb_tipo_documento

--- database.py
+++ database.py
@@ -1,19 +1,19 @@
 import psycopg2
 from sqlalchemy import create_engine, text
 
-
 def executa_sql_sem_resultados(conn_string: str, query_string: str):
     """Executa uma query sem retornar resultados."""
     try:
         engine = create_engine(conn_string)
-        with engine.raw_connection() as conn:
-            with conn.cursor() as cur:
-                cur.execute(text(query_string))
-                conn.commit()
-        print('Query executada com sucesso (sem resultados).')
+        conn = engine.raw_connection()
+        cur = conn.cursor()
+        cur.execute(text(query_string))
+        conn.commit()
+        cur.close()
+        conn.close()
+        print("Query executada com sucesso (sem resultados).")
     except Exception as e:
-        print(f'Erro ao executar a query sem resultados: {e}')
-
+        print(f"Erro ao executar a query sem resultados: {e}")
 
 def executa_sql_com_resultados(conn_string: str, query_string: str):
     """Executa uma query e retorna os resultados."""
@@ -24,17 +24,27 @@ def executa_sql_com_resultados(conn_string: str, query_string: str):
             resultados = cursor.fetchall()
         return resultados
     except Exception as e:
-        print(f'Erro ao executar a query e retornar resultados: {e}')
+        print(f"Erro ao executar a query e retornar resultados: {e}")
         return None
     finally:
         conn.close()
 
-
-def executa_sql(
-    conn_string: str, query_string: str, retorna_resultado: bool = True
-):
+def executa_sql_com_retorno(conn_string: str, query_string: str, retorna_resultado: bool):
     """Executa a query de acordo com a flag retorna_resultado e o tipo de retorno desejado."""
     if retorna_resultado:
         return executa_sql_com_resultados(conn_string, query_string)
     else:
         executa_sql_sem_resultados(conn_string, query_string)
+
+def executa_sql(conn_string: str, query_string: str):
+    try:
+        engine = create_engine(conn_string)
+        conn = engine.raw_connection()
+        cur = conn.cursor()
+        cur.execute(query_string)
+        conn.commit()
+        cur.close()
+        conn.close()
+
+    except Exception as e:
+        print(e)
