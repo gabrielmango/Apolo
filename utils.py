0953d68737d766d742a0f3c497452334879c5a82
gabrielhmango <gabrielhmango@gmail.com>
2025-01-16 09:38:50 -0300

    adicionar função carregar_de_json

--- utils.py
+++ utils.py
@@ -8,3 +8,14 @@ def salvar_em_json(lista, nome_arquivo):
         print(f'Dados salvos com sucesso no arquivo {nome_arquivo}.')
     except Exception as e:
         print(f'Erro ao salvar o arquivo JSON: {e}')
+
+
+def carregar_de_json(nome_arquivo):
+    try:
+        with open(nome_arquivo, 'r', encoding='utf-8') as arquivo:
+            dados = json.load(arquivo)
+        print(f'Dados carregados com sucesso do arquivo {nome_arquivo}.')
+        return dados
+    except Exception as e:
+        print(f'Erro ao carregar o arquivo JSON: {e}')
+        return []
