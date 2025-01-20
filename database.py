0df19a622fbb2a940495ef2f640ce0d2a90607ed
gabrielhmango <gabrielhmango@gmail.com>
2025-01-20 14:38:04 -0300

    funcao main e valores distintos de funcionalidade e projeto

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import psycopg2
 from sqlalchemy import create_engine, text
 
+
 def executa_sql_sem_resultados(conn_string: str, query_string: str):
     """Executa uma query sem retornar resultados."""
     try:
@@ -11,9 +12,10 @@ def executa_sql_sem_resultados(conn_string: str, query_string: str):
         conn.commit()
         cur.close()
         conn.close()
-        print("Query executada com sucesso (sem resultados).")
+        print('Query executada com sucesso (sem resultados).')
     except Exception as e:
-        print(f"Erro ao executar a query sem resultados: {e}")
+        print(f'Erro ao executar a query sem resultados: {e}')
+
 
 def executa_sql_com_resultados(conn_string: str, query_string: str):
     """Executa uma query e retorna os resultados."""
@@ -24,17 +26,21 @@ def executa_sql_com_resultados(conn_string: str, query_string: str):
             resultados = cursor.fetchall()
         return resultados
     except Exception as e:
-        print(f"Erro ao executar a query e retornar resultados: {e}")
+        print(f'Erro ao executar a query e retornar resultados: {e}')
         return None
     finally:
         conn.close()
 
-def executa_sql_com_retorno(conn_string: str, query_string: str, retorna_resultado: bool):
+
+def executa_sql_com_retorno(
+    conn_string: str, query_string: str, retorna_resultado: bool
+):
     """Executa a query de acordo com a flag retorna_resultado e o tipo de retorno desejado."""
     if retorna_resultado:
         return executa_sql_com_resultados(conn_string, query_string)
     else:
         executa_sql_sem_resultados(conn_string, query_string)
+
 
 def executa_sql(conn_string: str, query_string: str):
     try:
--- filserver/gerencia_tipo_documento.py
+++ filserver/gerencia_tipo_documento.py
@@ -1,5 +1,7 @@
 from dotenv import dotenv_values
 
+from utils.gerenciar_json import carregar_de_json
+
 config = dotenv_values('.env')
 
 ambientes_fileserver = {
@@ -7,6 +9,35 @@ ambientes_fileserver = {
     'tst': config['FILESERVER_POSTGRES_TST'],
     'hml': config['FILESERVER_POSTGRES_HML'],
     'preprod': config['FILESERVER_POSTGRES_PREPROD'],
-    'prod': config['FILESERVER_POSTGRES_PROD']
+    'prod': config['FILESERVER_POSTGRES_PROD'],
 }
 
+
+def main():
+    tipos_documentos = carregar_de_json(
+        r'filserver\data\tipo_documentos_cesv.json'
+    )
+
+    lista_funcionalidades = set()
+    lista_projetos = set()
+    for tipo in tipos_documentos:
+        for funcionalidade in tipo['funcionalidades']:
+            lista_funcionalidades.add(funcionalidade)
+        for projeto in tipo['projetos']:
+            lista_projetos.add(projeto)
+
+
+if __name__ == '__main__':
+    from datetime import datetime
+
+    print(f'\nProcesso iniciado: {datetime.now()} \n')
+    start_time = datetime.now()
+
+    main()
+
+    end_time = datetime.now()
+    print(f'\nProcesso finalizado: {datetime.now()} \n')
+
+    full_time = end_time - start_time
+    full_time = str(full_time).split('.')[0]
+    print(f'Tempo de execução: {full_time}')
