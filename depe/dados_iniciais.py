6995c267951870605529394b0e2a889f13f69938
gabrielhmango <gabrielhmango@gmail.com>
2025-01-21 10:23:35 -0300

    Refatora a funcao salvar dados da tabela tb_campo

--- depe/dados_iniciais.py
+++ depe/dados_iniciais.py
@@ -7,68 +7,51 @@ SCHEMA_DEPE = 'depe_bkp_inicial'
 NOME_ARQUIVO = 'depe/data/V07__DADOS_INICIAS_PROJETO_DEPE'
 
 
-def salva_arquivo_sql(ambiente):
-    dados_tb_campo = retorna_dados_tb_campo(string_depe[ambiente])
-    sql_insert_tb_campo = retorna_sql_insert_tb_campo(dados_tb_campo)
-    salvar_em_sql(sql_insert_tb_campo, NOME_ARQUIVO)
-
-
-def retorna_dados_tb_campo(ambiente: str):
+def retorna_sql_insert_tb_campo(ambiente: str):
     query = f"""
-        select
-            co_seq_campo
-            ,no_campo
-            ,tp_campo
-            ,ds_campo
-            ,qt_caracteres_codigo
-            ,nu_ordem_codigo
-            ,tp_valor_campo
-            ,co_uuid
-            ,fl_campo_obrigatorio
-            ,fl_destacar_campo_front_end
-            ,ds_intervencao_externa
-        from 
-            {SCHEMA_DEPE}.tb_campo tc
-        WHERE 
-            st_ativo = TRUE
-        ORDER BY 
-            co_seq_campo ASC;
+        SELECT
+            co_seq_campo, no_campo, tp_campo, ds_campo, qt_caracteres_codigo,
+            nu_ordem_codigo, tp_valor_campo, co_uuid, fl_campo_obrigatorio,
+            fl_destacar_campo_front_end, ds_intervencao_externa
+        FROM {SCHEMA_DEPE}.tb_campo
+        WHERE st_ativo = TRUE
+        ORDER BY co_seq_campo ASC;
     """
-    return consulta_para_lista(ambiente, query)
+    dados_tb_campo = consulta_para_lista(string_depe[ambiente], query)
 
-
-def retorna_sql_insert_tb_campo(dados_tb_campo) -> str:
-
-    sql_string = ''
-
-    for campo in dados_tb_campo:
-
-        campo['no_campo'] = campo['no_campo'].replace("'", '')
-        campo['ds_campo'] = campo['ds_campo'].replace("'", '')
-
-        sql = f"""
--- {campo['no_campo']}
+    sql_template = """
+-- {no_campo}
 INSERT INTO depe.tb_campo
 (co_seq_campo, no_campo, tp_campo, ds_campo, qt_caracteres_codigo, 
 nu_ordem_codigo, tp_valor_campo, fl_campo_obrigatorio, 
 fl_destacar_campo_front_end, ds_intervencao_externa,
 sg_projeto_modificador, sg_acao_modificadora, no_end_point_modificador, 
 st_ativo, dh_criacao, tp_operacao, nu_versao, co_uuid, co_uuid_1)
-SELECT {campo['co_seq_campo']}, '{campo['no_campo']}', '{campo['tp_campo']}', 
-'{campo['ds_campo']}', {campo['qt_caracteres_codigo']}, {campo['nu_ordem_codigo']}, 
-'{campo['tp_valor_campo']}', {campo['fl_campo_obrigatorio']}, 
-{campo['fl_destacar_campo_front_end']}, '{campo['ds_intervencao_externa']}', 
+SELECT {co_seq_campo}, '{no_campo}', '{tp_campo}', '{ds_campo}', {qt_caracteres_codigo}, 
+{nu_ordem_codigo}, '{tp_valor_campo}', {fl_campo_obrigatorio}, 
+{fl_destacar_campo_front_end}, '{ds_intervencao_externa}', 
 'INSERSAO_INICIAL', 'INSERSAO_INICIAL', 'INSERSAO_INICIAL', TRUE, now(), 'CREATE', 1, 
-'{campo['co_uuid']}', '60a75feb-0170-4f38-a2cc-e31269440a61'
-WHERE NOT EXISTS (SELECT 1 FROM depe.tb_campo WHERE co_seq_campo = {campo['co_seq_campo']} 
-AND no_campo = '{campo['no_campo']}');"""
-        sql_string += sql
+'{co_uuid}', '60a75feb-0170-4f38-a2cc-e31269440a61'
+WHERE NOT EXISTS (
+    SELECT 1 FROM depe.tb_campo WHERE co_seq_campo = {co_seq_campo} AND no_campo = '{no_campo}'
+);"""
 
-    return sql_string
+    return ''.join(
+        sql_template.format(
+            **{
+                key: (
+                    value.replace("'", '') if isinstance(value, str) else value
+                )
+                for key, value in campo.items()
+            }
+        )
+        for campo in dados_tb_campo
+    )
 
 
 def main():
-    salva_arquivo_sql('dev')
+    salvar_em_sql('-- Dados para a tabela tb_campo', NOME_ARQUIVO)
+    salvar_em_sql(retorna_sql_insert_tb_campo('dev'), NOME_ARQUIVO)
 
 
 if __name__ == '__main__':
--- utils/gerenciar_sql.py
+++ utils/gerenciar_sql.py
@@ -3,8 +3,10 @@ def salvar_em_sql(string, nome_arquivo):
     if '.sql' not in nome_arquivo:
         nome_arquivo = nome_arquivo + '.sql'
 
+    string = string + '\n'
+
     try:
-        with open(nome_arquivo, 'w', encoding='utf-8') as arquivo:
+        with open(nome_arquivo, 'a+', encoding='utf-8') as arquivo:
             arquivo.write(string)
     except Exception as e:
         print(f'Erro ao salvar o arquivo .sql: {e}')
