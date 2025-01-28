828656ab75d862bdccc1dcabe219af4ad3b49ca6
gabrielhmango <gabrielhmango@gmail.com>
2025-01-28 14:54:38 -0300

    Adiconar funcao de logging no utils

--- utils/setup_logging.py
+++ utils/setup_logging.py
@@ -1,11 +1,12 @@
-import os
 import logging
+import os
 from pathlib import Path
+
 
 def setup_logging(nome):
     script_name = os.path.basename(nome)
-    log_folder = "logs"
-    log_file = f"{log_folder}/{script_name}.log"
+    log_folder = 'logs'
+    log_file = f'{log_folder}/{script_name}.log'
 
     if not os.path.exists(log_folder):
         os.makedirs(log_folder)
@@ -15,9 +16,6 @@ def setup_logging(nome):
         format='%(asctime)s - %(levelname)s - %(message)s',
         handlers=[
             logging.FileHandler(log_file, encoding='utf-8'),
-            logging.StreamHandler() 
-        ]
+            logging.StreamHandler(),
+        ],
     )
-
-
-
