5fb2f342f9090c4919a76993c19a8f0e3628c28e
gabrielhmango <gabrielhmango@gmail.com>
2025-02-20 16:33:33 -0300

    valida coluna fl_principal_pessoa_externa na tabela tb_email

--- pessoa_externa/atualizar_contato.py
+++ pessoa_externa/atualizar_contato.py
@@ -1,11 +1,31 @@
+from database import executar_query
 from utils.ambientes import string_contato, string_pessoa_externa
 from utils.setup_logging import logging, setup_logging
 
 setup_logging(__file__)
 
 
-def main(ambiente: str = 'dev'):
-    ...
+def valida_coluna_contato_externa(ambiente):
+    query = f"""
+        SELECT column_name
+        FROM information_schema.columns
+        WHERE table_schema = 'contato' and table_name = 'tb_email'
+        AND column_name = 'fl_principal_pessoa_externa';
+    """
+
+    result = executar_query(True, query, string_contato[ambiente])
+
+    if result:
+        return True
+    return False
+
+
+def main(ambiente: str = 'tst'):
+
+    if valida_coluna_contato_externa(ambiente):
+        logging.info(
+            'Coluna fl_principal_pessoa_externa já existe na tabela tb_email'
+        )
 
 
 if __name__ == '__main__':
