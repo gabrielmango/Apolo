910a6159ec18e45387e0fcd6c92a8981a7a59862
gabrielhmango <gabrielhmango@gmail.com>
2025-02-21 19:26:05 -0300

    adiciona funcao carrega processos seletivos

--- cesv/atualiza_municipio.py
+++ cesv/atualiza_municipio.py
@@ -1,10 +1,30 @@
+from database import executar_query
+from utils.ambientes import string_cesv
 from utils.setup_logging import logging, setup_logging
 
 setup_logging(__file__)
 
 
-def main():
-    ...
+def retorna_processos_seletivos(ambiente):
+    return executar_query(
+        True,
+        """
+        select 
+            co_seq_processo_seletivo as id,
+            ds_titulo_processo_seletivo as titulo
+        from cesv.tb_processo_seletivo tps 
+        where fl_processo_seletivo_antigo
+        order by co_seq_processo_seletivo asc;
+        """,
+        string_cesv[ambiente],
+    )
+
+
+def main(ambiente: str = 'preprod'):
+    dados = retorna_processos_seletivos(ambiente)
+
+    for dado in dados:
+        print(dado)
 
 
 if __name__ == '__main__':
