a5d1dcea3117abe574972a8be9b23c142642b166
gabrielhmango <gabrielhmango@gmail.com>
2025-03-07 10:08:09 -0300

    retorna candidaturas

--- cesv/telefone_email_principal.py
+++ cesv/telefone_email_principal.py
@@ -1,10 +1,33 @@
+from database import executar_query
+from utils.ambientes import string_cesv
 from utils.setup_logging import logging, setup_logging
 
 setup_logging(__file__)
 
 
+def retorna_candidaturas(ambiente):
+    return executar_query(
+        True,
+        """
+        SELECT
+            co_uuid_2 AS uuid,
+            st_candidatura as status
+        FROM
+            cesv.tb_candidatura
+        """,
+        string_cesv[ambiente],
+    )
+
+
 def main(ambiente: str = 'prod'):
-    ...
+    logging.info(f'Iniciando processo para {ambiente}')
+    candidaturas = retorna_candidaturas(ambiente)
+    logging.info(
+        f'Candidaturas encontradas em {ambiente}: {len(candidaturas)}'
+    )
+
+    for candidatura in candidaturas:
+        logging.info(f'UUID: {candidatura["uuid"]}')
 
 
 if __name__ == '__main__':
--- utils/ambientes.py
+++ utils/ambientes.py
@@ -75,10 +75,10 @@ string_localizacao = {
 
 string_patrimonio = {'dev': config['PATRIMONIO_DEV']}
 
-string_geral_pessoa = {
-    'dev': config['GERAL_PESSOA_DEV'],
-    'tst': config['GERAL_PESSOA_TST'],
-    'hml': config['GERAL_PESSOA_HML'],
-    'preprod': config['GERAL_PESSOA_PREPROD'],
-    'prod': config['GERAL_PESSOA_PROD'],
-}
+# string_geral_pessoa = {
+#     'dev': config['GERAL_PESSOA_DEV'],
+#     'tst': config['GERAL_PESSOA_TST'],
+#     'hml': config['GERAL_PESSOA_HML'],
+#     'preprod': config['GERAL_PESSOA_PREPROD'],
+#     'prod': config['GERAL_PESSOA_PROD'],
+# }
