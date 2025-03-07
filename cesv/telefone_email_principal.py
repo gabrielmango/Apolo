2e1277614050dc324f6394496768ceed7fae8890
gabrielhmango <gabrielhmango@gmail.com>
2025-03-07 10:40:40 -0300

    retorna candidatura sem email principal

--- cesv/telefone_email_principal.py
+++ cesv/telefone_email_principal.py
@@ -1,5 +1,5 @@
 from database import executar_query
-from utils.ambientes import string_cesv
+from utils.ambientes import string_cesv, string_contato
 from utils.setup_logging import logging, setup_logging
 
 setup_logging(__file__)
@@ -14,8 +14,26 @@ def retorna_candidaturas(ambiente):
             st_candidatura as status
         FROM
             cesv.tb_candidatura
+        WHERE st_candidatura <> 'RASCUNHO'
         """,
         string_cesv[ambiente],
+    )
+
+
+def retorna_email(ambiente, uuid):
+    return executar_query(
+        True,
+        f"""
+        SELECT
+            co_seq_email as id,
+            fl_email_principal as principal
+        FROM
+            contato.tb_email
+        WHERE
+            co_uuid_2 = '{uuid}' and
+            st_ativo and fl_email_principal
+        """,
+        string_contato[ambiente],
     )
 
 
@@ -28,6 +46,13 @@ def main(ambiente: str = 'prod'):
 
     for candidatura in candidaturas:
         logging.info(f'UUID: {candidatura["uuid"]}')
+
+        email = retorna_email(ambiente, candidatura['uuid'])
+
+        if not email:
+            logging.info(
+                f'Email principal não encontrado para {candidatura["uuid"]}'
+            )
 
 
 if __name__ == '__main__':
