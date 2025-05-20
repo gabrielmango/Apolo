0a2feb37e4781f3b3376af61a27e2d79ee68237c
gabrielhmango <gabrielhmango@gmail.com>
2025-05-20 11:08:00 -0300

    adiciona modulo para relatorios do cesv

---
+++ cesv/relatorios/documentacao_processo_seletivo.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+from database import executar_query, list_to_sql
+from utils.ambientes import (string_cesv, string_contato, string_fileserver,
+                             string_geral_pessoa, string_localizacao)
+from utils.setup_logging import logging, setup_logging
+
+setup_logging(__file__)
+
+
+def main(ambiente: str = 'prod'):
+
+    logging.info(f'Buscando candidaturas rascunho em {ambiente}')
+
+    logging.info(f'Processo finalizado em {ambiente}')
+
+
+if __name__ == '__main__':
+    from datetime import datetime
+
+    logging.info('Processo iniciado')
+    start_time = datetime.now()
+
+    main()
+
+    end_time = datetime.now()
+    tempo_execucao = str(end_time - start_time).split('.')[0]
+    logging.info(f'Tempo de execução: {tempo_execucao}')
+    logging.info('Processo finalizado\n')
