a643cf8f5124bb8a0ee3d7fc828070db12d8a033
gabrielhmango <gabrielhmango@gmail.com>
2025-06-10 14:41:07 -0300

    feat: add class DeleteInternalUser

--- institucional/delete_internal_user.py
+++ institucional/delete_internal_user.py
@@ -3,9 +3,14 @@ from utils.setup_logging import LogHandler
 handler = LogHandler('delete_internal_user')
 
 
+class DeleteInternalUser:
+    def __init__(self, number_cpf: str) -> None:
+        self._number_cpf = number_cpf
+
+
 @handler
 def main():
-    print(__file__)
+    user = DeleteInternalUser('12314411609')
 
 
 if __name__ == '__main__':
